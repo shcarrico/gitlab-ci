f4a30036c53faae65831f1bfb9f90b9edb3631df
Stan Carrico <shcarrico@gmail.com>
2014-09-19 22:33:11 -0500

    removed admin checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_admin!
-    unless current_user && current_user.is_admin
+    unless current_user
       redirect_to new_user_sessions_path
       return
     end
